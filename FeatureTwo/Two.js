e3671393676cd4b26cd42f5839d4ac9e6b7493e6
sourav-18 <souravhd2@gmail.com>
2023-12-02 15:54:13 +0530

    featureTwo create and also Two.js file

---
+++ FeatureTwo/Two.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Two = () => {
+  return (
+    <View>
+      <Text>Two</Text>
+    </View>
+  )
+}
+
+export default Two
+
+const styles = StyleSheet.create({})
